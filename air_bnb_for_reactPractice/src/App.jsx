ed968f41dda76bab655999cd87ffdf3e1d2b7325
Rahul <rahulkrish321123@gmail.com>
2023-07-11 17:52:03 +0530

    Hero completed

--- air_bnb_for_reactPractice/src/App.jsx
+++ air_bnb_for_reactPractice/src/App.jsx
@@ -1,9 +1,14 @@
 
 import './App.css'
 import Nav from "./comp/nav.jsx";
+import Hero from "./comp/hero.jsx";
+
 function App() {
   return (
+      <div>
     <Nav/>
+      <Hero/>
+      </div>
   )
 }
 
---
+++ air_bnb_for_reactPractice/src/comp/hero.jsx
@@ -0,0 +1,18 @@
+import grid from "../assets/photo-grid.png"
+
+
+export default function Hero(){
+    return(
+        <div id={"hero-container"}>
+            <div className={"hero_image"}>
+                <img src={
+                    grid
+                } alt={"hero img"}/>
+            </div>
+            <div className={"hero_text"}>
+                <p>Online Experiences</p>
+                <p> unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
+        </div>
+    )
+}
